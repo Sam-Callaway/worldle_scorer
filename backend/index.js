d6e967622c8d599d85ea919d2b0f7fb558803094
Sam Callaway <calls002@gmail.com>
2023-10-12 16:55:17 +0100

    getting hidden keys to work

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+require('dotenv').config()
+
+const password = process.env.MONGODBPASSWORD
 const url = 'mongodb+srv://calls002:'+password+'@cluster0.iraeblh.mongodb.net/?retryWrites=true&w=majority'
 
 app.get('/today', (req, res) => {
@@ -17,3 +20,4 @@ app.get('/history', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
